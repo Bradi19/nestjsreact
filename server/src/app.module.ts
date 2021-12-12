4a87a0ea0fa24c8987fab25c386a39f2a483fc15
Your Name <you@example.com>
2021-12-12 19:19:16 +0300

    ol

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ItemsModule } from './items/items.module';
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {ItemsModule} from './items/items.module';
 
 @Module({
-  imports: [ItemsModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ItemsModule,
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'Kolyan2010@',
+            database: 'auctions',
+            logging: true,
+            synchronize: true,
+        })],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
